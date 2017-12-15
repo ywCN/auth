ed16fc9634d7fb0edd54d463fe6f60d27756b405
ywCN <wangyouhao90@gmail.com>
2017-12-14 22:40:27 -0500

    set up firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import firebase from 'firebase';
+import { Header } from './components/common'; // should import our own stuffs at bottom
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyBBQ5HG9QpXI6wLBaAMAV9KeAS4Qc0e0ZQ',
+            authDomain: 'auth-a21c9.firebaseapp.com',
+            databaseURL: 'https://auth-a21c9.firebaseio.com',
+            projectId: 'auth-a21c9',
+            storageBucket: 'auth-a21c9.appspot.com',
+            messagingSenderId: '857751852568'
+          });
+    }
+
     render() {
         return (
             <View>
