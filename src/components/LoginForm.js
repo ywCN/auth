1bd83fd82b0f2b3d45890d6fb800c38f5dd66d29
ywCN <wangyouhao90@gmail.com>
2017-12-15 00:05:56 -0500

    added password input box

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardSection, Button, Input } from './common';
 
 class LoginForm extends Component {
-    state = { email: '' };
+    state = { email: '', password: '' };
 
     render() {
         return (
@@ -16,7 +16,16 @@ class LoginForm extends Component {
                         onChangeText={email => this.setState({ email })} //{email: email}
                     />
                 </CardSection>
-                <CardSection />
+
+                <CardSection>
+                    <Input
+                        // these stuffs are passed as props
+                        placeholder='password'
+                        label='Password'
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                    />
+                </CardSection>
 
                 <CardSection>
                     <Button>
