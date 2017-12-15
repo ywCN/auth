9dd42bf8857927dcd02cf14a02cbab4d6ae1158f
ywCN <wangyouhao90@gmail.com>
2017-12-15 09:25:00 -0500

    added style

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -56,4 +56,12 @@ class LoginForm extends Component {
     }
 }
 
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
+
 export default LoginForm;
