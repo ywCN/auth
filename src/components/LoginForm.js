f52fb86bb16264a6db4b58063d65de9a258c0707
ywCN <wangyouhao90@gmail.com>
2017-12-15 09:23:41 -0500

    added handeler for login and creating account

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import firebase from 'firebase';
 import { Card, CardSection, Button, Input } from './common';
 
 class LoginForm extends Component {
-    state = { email: '', password: '' };
+    state = { email: '', password: '', error: '' };
 
     onButtonPress() {
         const { email, password } = this.state;
-        firebase.auth().signInWithEmailAndPassword(email, password);
+        // this is an async operation which returns a Promise
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({ error: 'Authentication Failed.' });
+                    });
+            });
     }
 
     render() {
@@ -33,6 +41,10 @@ class LoginForm extends Component {
                         secureTextEntry // just listing this means true
                     />
                 </CardSection>
+
+                <Text>
+                    {this.state.error}
+                </Text>
 
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>
