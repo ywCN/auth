e7452cebdc787985e15d3f80e38413376b293e36
ywCN <wangyouhao90@gmail.com>
2017-12-15 09:10:57 -0500

    added callback function

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import { Card, CardSection, Button, Input } from './common';
 
 class LoginForm extends Component {
     state = { email: '', password: '' };
+
+    onButtonPress() {
+        const { email, password } = this.state;
+        firebase.auth().signInWithEmailAndPassword(email, password);
+    }
 
     render() {
         return (
@@ -29,7 +35,7 @@ class LoginForm extends Component {
                 </CardSection>
 
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Log In
                     </Button>
                 </CardSection>
